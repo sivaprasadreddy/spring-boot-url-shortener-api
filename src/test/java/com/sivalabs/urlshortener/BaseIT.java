6e7efa0f7619022c36723f9e402304d316f5b62b
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-08-04 17:28:28 +0530

    feat: using arconia dev services for postgresql LGTM

--- src/test/java/com/sivalabs/urlshortener/BaseIT.java
+++ src/test/java/com/sivalabs/urlshortener/BaseIT.java
@@ -3,13 +3,11 @@ package com.sivalabs.urlshortener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Import(TestcontainersConfiguration.class)
 @AutoConfigureMockMvc
 public abstract class BaseIT {
     @Autowired
